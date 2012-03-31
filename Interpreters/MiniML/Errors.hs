94b7ac974296f388d7ad99272e8356a85ce2d06c
Krzysztof Sakwerda <krzysztof.sakwerda@simplificator.com>
2012-03-31 18:15:16 +0200

    Cleaning unused errors, proper kinding

--- Interpreters/MiniML/Errors.hs
+++ Interpreters/MiniML/Errors.hs
@@ -1,25 +1,7 @@
 module Interpreters.MiniML.Errors where
+  too_many_arguments what e1 e2 = what ++ " " ++ show e1 ++ " is applied to to many arguments in: " ++ show e2
 
-  unbound_variable v = "Unbound variable: " ++ show v
-
-  unbound_type_variable v = "Unbound type variable: " ++ show v
-
-  invalid_kind expr expected (subexpr, kind) = "Invalid kinding in: " ++ show expr ++
-                                                  "\n\texpected expression of kind: " ++ show expected ++
-                                                  "\n\tgot expression: " ++ show subexpr ++ 
-                                                  "\n\tof kind: " ++ show kind
-
-  invalid_type_constr expr type_constr expected actual = "Invalid kinding in: " ++ show expr ++
-                                                         "\n\t" ++ show type_constr ++ " expects " ++ show expected ++ " arguments, got " ++ show actual
-
-  non_distinct_names expr = "Typing error in: " ++ show expr ++
-                            "\n\tnon distinct names"
-
-  invalid_type expr expected (subexpr, tp) = "Invalid typing in: " ++ show expr ++
-                                             "\n\texpected expression of type: " ++ show expected ++
-                                             "\n\tgot expression: " ++ show subexpr ++
-                                             "\n\tof type: " ++ show tp
-
-  empty_pattern_matching = "Empty pattern matching"
-
-  empty_let_rec_binding = "Empty letrec binding"
+  invalid what expr expected (subexpr, actual) = "Invalid " ++ show what ++ " in: " ++ show expr ++
+                                                  "\n\texpected: " ++ show expected ++
+                                                  "\n\tgot: " ++ show subexpr ++ 
+                                                  "\n\tof " ++ show what ++ ": " ++ show actual
--- Interpreters/MiniML/Kinding.hs
+++ Interpreters/MiniML/Kinding.hs
@@ -1,54 +1,40 @@
-module Interpreters.MiniML.Kinding where
+module Interpreters.MiniML.Kinding (kind_of_type_expr) where
   import Data.List
   import Data.Maybe
 
   import Interpreters.MiniML.Syntax
+  import Interpreters.MiniML.PrettyPrint
   import Interpreters.MiniML.Errors
 
   kind_of_type_constr :: TypeConstr -> Kind
   kind_of_type_constr Int   = K_Type
-  kind_of_type_constr Float = K_Type
   kind_of_type_constr Bool  = K_Type
   kind_of_type_constr Unit  = K_Type
-  kind_of_type_constr List  = K_Arrow 1
-  kind_of_type_constr Ref   = K_Arrow 1
+  kind_of_type_constr List  = K_Arrow K_Type K_Type
+  kind_of_type_constr Ref   = K_Arrow K_Type K_Type
 
-  kind_of_type_expr :: Env -> TypeExpr -> Either String Kind
-  kind_of_type_expr e (TE_Var v)
-    | v `is_bound_in` e = Right K_Type
-    | otherwise         = Left $ unbound_type_variable v
-  kind_of_type_expr e te@(TE_Arrow e1 e2) =
-    case (kind_of_type_expr e e1, kind_of_type_expr e e1) of
+  kind_of_type_expr :: TypeExpr -> Either String Kind
+  kind_of_type_expr (TE_Var _) = Right K_Type
+  kind_of_type_expr te@(TE_Arrow e1 e2) =
+    case (kind_of_type_expr e1, kind_of_type_expr e1) of
       (Left err,     _)            -> Left err
       (_,            Left err)     -> Left err
       (Right K_Type, Right K_Type) -> Right K_Type
-      (Right K_Type, Right k2)     -> Left $ invalid_kind te K_Type (e2, k2)
-      (Right k1,     _)            -> Left $ invalid_kind te K_Type (e1, k1)
-  kind_of_type_expr e te@(TE_Tuple es)
-    | all (\x -> kind_of_type_expr e x == Right K_Type) es = Right K_Type
-    | otherwise                                            = Left $ invalid_kind te K_Type (expr, kind_of_type_expr e expr)
+      (Right K_Type, Right k2)     -> Left $ invalid "kind" te K_Type (e2, k2)
+      (Right k1,     _)            -> Left $ invalid "kind" te K_Type (e1, k1)
+  kind_of_type_expr te@(TE_Tuple es)
+    | all (\x -> kind_of_type_expr x == Right K_Type) es = Right K_Type
+    | otherwise                                          = Left $ invalid "kind" te K_Type (expr, kind_of_type_expr expr)
     where
-      expr = fromJust $ find (\x -> kind_of_type_expr e x /= Right K_Type) es
-  kind_of_type_expr e te@(TE_Constr es tc) =
-    case (kind_of_type_constr tc, length es) of
-      (K_Type,    0) -> Right K_Type
-      (K_Type,    n) -> Left $ invalid_type_constr te tc 0 n
-      (K_Arrow n, z) ->
-        if n == toInteger z
-        then 
-          if all (\x -> kind_of_type_expr e x == Right K_Type) es
-          then Right K_Type
-          else Left $ invalid_kind te K_Type (expr, kind_of_type_expr e expr)
-        else Left $ invalid_type_constr te tc n z
-        where
-          expr = fromJust $ find (\x -> kind_of_type_expr e x /= Right K_Type) es
-
-  kind_of_type_scheme :: Env -> TypeScheme -> Either String Kind
-  kind_of_type_scheme e (TS_Forall te) = kind_of_type_expr (e ++ map EB_TV (free_type_vars e te)) te
+      expr = fromJust $ find (\x -> kind_of_type_expr x /= Right K_Type) es
+  kind_of_type_expr te@(TE_Constr es tc) = apply es $ kind_of_type_constr tc
     where
-      free_type_vars e (TE_Var v)
-        | v `is_bound_in` e              = []
-        | otherwise                      = [v]
-      free_type_vars e (TE_Arrow e1 e2)  = (free_type_vars e e1) ++ (free_type_vars e e2)
-      free_type_vars e (TE_Tuple es)     = concatMap (free_type_vars e) es 
-      free_type_vars e (TE_Constr es tc) = concatMap (free_type_vars e) es
+      apply [] fk     = Right fk
+      apply (k:ks) fk =
+        case (kind_of_type_expr k, fk) of
+          (Left e, _)               -> Left e
+          (Right kk, K_Arrow k1 k2) ->
+            if   kk == k1
+            then apply ks k2
+            else Left $ invalid "kind" te k1 (k, kk)
+          (_,       k)              -> Left $ too_many_arguments "Type constructor" tc te
