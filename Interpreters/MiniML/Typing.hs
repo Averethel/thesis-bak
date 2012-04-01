a5c3dec4b5adfd3be90a24f8ae637b8be7df2630
Krzysztof Sakwerda <krzysztof.sakwerda@simplificator.com>
2012-04-01 15:18:19 +0200

    Initial work on typing

--- Interpreters/MiniML/Typing.hs
+++ Interpreters/MiniML/Typing.hs
@@ -1,73 +1,52 @@
 module Interpreters.MiniML.Typing where
   import Interpreters.MiniML.Syntax
+  import Interpreters.MiniML.PrettyPrint
+  import Interpreters.MiniML.Kinding
+  import Interpreters.MiniML.Errors
 
-  type Kind = Integer
+  fresh_type_var :: [TypeVar] -> TypeVar
+  fresh_type_var = ("a" ++).show.length
 
-  data EnvBinding =
-      EB_TV TypeVar
-    | EB_TE ValueName TypeExpr
-    deriving (Show, Eq)
-
-  type Env = [EnvBinding]
-
-  is_bound_in :: TypeVar -> Env -> Bool
-  is_bound_in _ []                = False
-  is_bound_in v ((EB_TE _ _):ebs) = v `is_bound_in` ebs
-  is_bound_in v ((EB_TV tv):ebs)
-    | v == tv                     = True
-    | otherwise                   = v `is_bound_in` ebs
-
-
-  kind_of_type_constr :: TypeConstr -> Kind
-  kind_of_type_constr Int   = 0
-  kind_of_type_constr Float = 0
-  kind_of_type_constr Bool  = 0
-  kind_of_type_constr Unit  = 0
-  kind_of_type_constr List  = 1
-  kind_of_type_constr Ref   = 1
-
-  kind_of_type_expr :: Env -> TypeExpr -> Kind
-  kind_of_type_expr e (TE_Var v)
-    | v `is_bound_in` e                                  = 0
-  kind_of_type_expr e (TE_Arrow e1 e2)
-    | kind_of_type_expr e e1 == 0 &&
-      kind_of_type_expr e e1 == 0                        = 0
-  kind_of_type_expr e (TE_Tuple es)
-    | all (\x -> kind_of_type_expr e x == 0) es          = 0
-  kind_of_type_expr _ (TE_Constr [] tc)
-    | kind_of_type_constr tc == 0                        = 0
-  kind_of_type_expr e (TE_Constr es tc)
-    | kind_of_type_constr tc == (toInteger.length $ es) &&
-      all (\x -> kind_of_type_expr e x == 0) es          = 0
-
-  type_of_constant :: Env -> TypeExpr -> Constant -> TypeExpr
-  type_of_constant _ _ (C_Int _)   = TE_Constr [] Int
-  type_of_constant _ _ (C_Float _) = TE_Constr [] Float
-  type_of_constant _ _ C_False     = TE_Constr [] Bool
-  type_of_constant _ _ C_True      = TE_Constr [] Bool
-  type_of_constant e t C_Nil
-    | kind_of_type_expr e t == 0   = TE_Constr [t] List
-  type_of_constant _ _ C_Unit      = TE_Constr [] Unit
-
-  type_of_unary_primitive :: Env -> TypeExpr -> UnaryPrim -> TypeExpr
-  type_of_unary_primitive _ _ U_Not   = TE_Arrow (TE_Constr [] Bool) (TE_Constr [] Bool)
-  type_of_unary_primitive e t U_Ref
-    | kind_of_type_expr e t == 0      = TE_Arrow t (TE_Constr [t] Ref)
-  type_of_unary_primitive e t U_Deref = 
-    | kind_of_type_expr e t == 0      = TE_Arrow (TE_Constr [t] Ref) t
-  type_of_unary_primitive _ _ U_Minus = TE_Arrow (TE_Constr [] Int) (TE_Constr [] Int)
-
-  type_of_binary_primitive :: Env -> TypeExpr -> BinaryPrim -> TypeExpr
-  type_of_binary_primitive e t B_Eq
-    | kind_of_type_expr e t == 0         = TE_Arrow t (TE_Arrow t Bool)
-  type_of_binary_primitive _ _ B_I_Plus  = TE_Arrow Int (TE_Arrow Int Bool)
-  type_of_binary_primitive _ _ B_I_Minus = TE_Arrow Int (TE_Arrow Int Bool)
-  type_of_binary_primitive _ _ B_I_Mult  = TE_Arrow Int (TE_Arrow Int Bool)
-  type_of_binary_primitive _ _ B_I_Div   = TE_Arrow Int (TE_Arrow Int Bool)
-  type_of_binary_primitive _ _ B_F_Plus  = TE_Arrow Float (TE_Arrow Float Bool)
-  type_of_binary_primitive _ _ B_F_Minus = TE_Arrow Float (TE_Arrow Float Bool)
-  type_of_binary_primitive _ _ B_F_Mult  = TE_Arrow Float (TE_Arrow Float Bool)
-  type_of_binary_primitive _ _ B_F_Div   = TE_Arrow Float (TE_Arrow Float Bool)
-  type_of_binary_primitive t e B_Assign
-    | kind_of_type_expr e t == 0         = TE_Arrow (TE_Constr [t] Ref) (TE_Arrow t Bool)
+  type_of_constant :: [TypeVar] -> Constant -> (([TypeVar], Constraints, SimpleConstraints), TypeExpr)
+  type_of_constant vs (C_Int n) = (([],  [], []), TE_Constr [] Int)
+  type_of_constant vs C_False   = (([],  [], []), TE_Constr [] Bool)
+  type_of_constant vs C_True    = (([],  [], []), TE_Constr [] Bool)
+  type_of_constant vs C_Nil     = (([v], [], []), TE_Constr [TE_Var v] List) where
+    v = fresh_type_var vs
+  type_of_constant vs C_Unit    = (([],  [], []), TE_Constr [] Unit)
   
+  type_of_unary_primitive :: [TypeVar] -> UnaryPrim -> (([TypeVar], Constraints, SimpleConstraints), TypeExpr)
+  type_of_unary_primitive vs U_Not     = (([],  [], []), TE_Arrow (TE_Constr [] Bool) (TE_Constr [] Bool))
+  type_of_unary_primitive vs U_Ref     = (([v], [], []), TE_Arrow (TE_Var v) (TE_Constr [TE_Var v] Ref)) where
+    v = fresh_type_var vs
+  type_of_unary_primitive vs U_Deref   = (([v], [], []), TE_Arrow (TE_Constr [TE_Var v] Ref) (TE_Var v)) where
+    v = fresh_type_var vs
+  type_of_unary_primitive vs U_I_Minus = (([],  [], []), TE_Arrow (TE_Constr [] Int) (TE_Constr [] Int))
+
+  type_of_binary_primitive :: [TypeVar] -> BinaryPrim -> (([TypeVar], Constraints, SimpleConstraints), TypeExpr)
+  type_of_binary_primitive vs B_Eq      = (([v], [], [TE_Var v]), TE_Arrow (TE_Var v) (TE_Arrow (TE_Var v) (TE_Constr [] Bool))) where
+    v = fresh_type_var vs
+  type_of_binary_primitive vs B_I_Plus  = (([],  [], []),         TE_Arrow (TE_Constr [] Int) (TE_Arrow (TE_Constr [] Int) (TE_Constr [] Int)))
+  type_of_binary_primitive vs B_I_Minus = (([],  [], []),         TE_Arrow (TE_Constr [] Int) (TE_Arrow (TE_Constr [] Int) (TE_Constr [] Int)))
+  type_of_binary_primitive vs B_I_Mult  = (([],  [], []),         TE_Arrow (TE_Constr [] Int) (TE_Arrow (TE_Constr [] Int) (TE_Constr [] Int)))
+  type_of_binary_primitive vs B_I_Div   = (([],  [], []),         TE_Arrow (TE_Constr [] Int) (TE_Arrow (TE_Constr [] Int) (TE_Constr [] Int)))
+  type_of_binary_primitive vs B_Assign  = (([v], [], []),         TE_Arrow (TE_Constr [TE_Var v] Ref) (TE_Arrow (TE_Var v) (TE_Constr [] Unit))) where
+    v = fresh_type_var vs
+
+  type_and_bindings_of_pattern :: [TypeVar] -> Pattern -> (([TypeVar], Constraints, SimpleConstraints), TypeExpr, Env)
+  type_and_bindings_of_pattern vs (P_Val vn)         = (([v], [], []), TE_Var v, [(vn, TE_Var v)]) where
+    v = fresh_type_var vs
+  type_and_bindings_of_pattern vs P_Wildcard         = (([v], [], []), TE_Var v, []) where
+    v = fresh_type_var vs
+  type_and_bindings_of_pattern vs (P_Const c)        = ((vs', c', s'), tp, []) where
+    ((vs', c', s'), tp) = type_of_constant vs c
+  -- parser ensures that there are at least two elems in a tuple
+  type_and_bindings_of_pattern vs (P_Tuple (p:ps))   = ((vs'', c'', s''), TE_Tuple $ reverse tes, e'') where
+    ((vs', c', s'), tp, e')      = type_and_bindings_of_pattern vs p
+    ((vs'', c'', s''), tes, e'') = foldl generate ((vs', c', s'), [tp], e') ps
+    generate :: (([TypeVar], Constraints, SimpleConstraints), [TypeExpr], Env) -> Pattern -> (([TypeVar], Constraints, SimpleConstraints), [TypeExpr], Env)
+    generate ((vs, cs, sc), ts, e) p = ((vs' ++ vs, cs' ++ cs, sc' ++ sc), tp:ts, e' ++ e) where
+      ((vs', cs', sc'), tp, e') = type_and_bindings_of_pattern vs p
+  type_and_bindings_of_pattern vs (P_Cons p1 p2)     = ((vs'', (te2, TE_Constr [te1] List):(cs1 ++ cs2), sc1 ++ sc2), te2, e1 ++ e2) where
+    ((vs',  cs1, sc1), te1, e1) = type_and_bindings_of_pattern vs p1
+    ((vs'', cs2, sc2), te2, e2) = type_and_bindings_of_pattern vs' p1
