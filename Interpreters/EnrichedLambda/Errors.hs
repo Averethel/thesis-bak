5805bb46927bad6d5ee06893fd2d0d8ea81111ff
Krzysztof Sakwerda <krzysztof.sakwerda@simplificator.com>
2012-05-01 14:44:23 +0200

    Errors

---
+++ Interpreters/EnrichedLambda/Errors.hs
@@ -0,0 +1,12 @@
+module Interpreters.EnrichedLambda.Errors where
+  unbound_variable v = "Unbound variable: " ++ show v
+  
+  cannot_unify te1 te2 = "Cannot unify: " ++ show te1 ++ "\n\twith: " ++ show te2
+  
+  cannot_compare te = "Cannot compare expressions of non simple type: " ++ show te
+  
+  memory_full = "Memory full"
+  
+  division_by_0 = "Division by 0"
+  
+  match_failure = "Match failure"
