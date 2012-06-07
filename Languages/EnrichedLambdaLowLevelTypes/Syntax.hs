c695c24106323ffe246a49ea8f589f0d21b31c55
Krzysztof Sakwerda <krzysztof.sakwerda@simplificator.com>
2012-06-07 21:07:48 +0200

    + Syntax of enriched lambda calculus with low level types

---
+++ Languages/EnrichedLambdaLowLevelTypes/Syntax.hs
@@ -0,0 +1,63 @@
+module Languages.EnrichedLambdaLowLevelTypes.Syntax where
+  data Tag = 
+      Tg_Int
+    | Tg_Pointer
+    | Tg_StaticPointer
+    | Tg_Nil
+    | Tg_Cons
+    | Tg_True
+    | Tg_False
+    | Tg_Unit
+    | Tg_Pair
+    deriving (Show, Eq)
+  
+  data Struct = 
+      S_Ref Expr
+    | S_Str Tag Integer [Struct]
+    | S_Ptr Integer
+    | S_StaticPtr Integer
+    | Null
+    deriving (Show, Eq)
+  
+  data Prim = 
+      P_AllocPair
+    | P_AllocList
+    | P_Not
+    | P_Ref
+    | P_Deref
+    | P_Fst
+    | P_Snd
+    | P_Head
+    | P_Tail
+    | P_Empty
+    | P_Eq
+    | P_Plus
+    | P_Minus
+    | P_Mult
+    | P_Div
+    | P_Assign
+    deriving (Show, Eq)
+  
+  data Expr = 
+      E_Prim Prim
+    | E_Val String
+    | E_Struct Struct
+    | E_ITE Expr Expr Expr
+    | E_Seq Expr Expr
+    | E_Let String Expr Expr
+    | E_Letrec String Expr Expr
+    | E_Apply Expr Expr
+    | E_Function String Expr
+    | E_MatchFailure
+    deriving (Show, Eq)
+  
+  data Type = 
+      T_Int
+    | T_Bool
+    | T_Unit
+    | T_Var String
+    | T_List Type
+    | T_Ref Type
+    | T_Pair Type Type
+    | T_Arrow Type Type
+    deriving (Show, Eq)
