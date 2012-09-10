6afd4bac2f25784c80eb0cafd6b6a3bbfeef958a
Krzysztof Sakwerda <ksakwerda@gmail.com>
2012-09-10 21:04:36 +0200

    - Removed old translation to EnrichedLambda

--- Compiler/Translations/MLtoEL.hs
+++
@@ -1,6 +0,0 @@
-module Compiler.Translations.MLtoEL (translate_to_enriched_lambda) where
-  import Compiler.Translations.MLtoEL.ExpressionsToDefinitions
-  import Compiler.Translations.MLtoEL.UniqueNamesEnforcer
-  import Compiler.Translations.MLtoEL.TranslationToEL
-
-  translate_to_enriched_lambda = program_to_enriched_lambda . enforce_unique_names . expressions_to_definitions
