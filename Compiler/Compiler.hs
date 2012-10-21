fa835b30d42e07d77a693e2b96e40f7ac36f3bd4
Krzysztof Sakwerda <ksakwerda@gmail.com>
2012-10-21 23:08:07 +0200

    + Correct module name and simplifying matchings in compiler

--- Compiler/Compiler.hs
+++ Compiler/Compiler.hs
@@ -1,15 +1,15 @@
-module Compiler where
+module Compiler.Compiler where
   import Compiler.Passes.EnforceUniqueNames
   import Compiler.Passes.ExpressionsToDefinitions
   import Compiler.Passes.EliminateWildcards
   import Compiler.Passes.FoldTuples
-  --import Compiler.Passes.SimplifyPatternMatchings
+  import Compiler.Passes.SimplifyPatternMatchings
   --import Compiler.Passes.TranslationToEL
 
   import Languages.MiniML.PrettyPrint
 
   compile = 
-    --simplify_pattern_matchings .
+    simplify_pattern_matchings .
     fold_tuples . 
     eliminate_wildcards .
     translate_expressions_to_definitions . 
