68a6f7b7e0aee52ee4a476fb3cca49ff937c7bf5
Krzysztof Sakwerda <ksakwerda@gmail.com>
2012-11-04 23:22:39 +0100

    + Translation to Enriched Lambda finished

--- Compiler.hs
+++ Compiler.hs
@@ -1,7 +1,9 @@
 module Compiler where
   import Compiler.Preprocess
   import Compiler.CompilePattetnMatching
+  import Compiler.TranslationToEL
 
   compile = 
+    program_to_enriched_lambda .
     compile_pattern_matching .
     preprocess
