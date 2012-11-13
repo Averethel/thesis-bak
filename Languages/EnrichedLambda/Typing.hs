2a4586b6bdd38f4270165b23c7fa1b73976c67fc
Krzysztof Sakwerda <ksakwerda@gmail.com>
2012-11-13 23:53:44 +0100

    + Added typing finctions required by language class

--- Languages/EnrichedLambda/Typing.hs
+++ Languages/EnrichedLambda/Typing.hs
@@ -114,7 +114,7 @@ module Languages.EnrichedLambda.Typing where
   extendEnv :: (MonadState (InterpreterState Type) m, MonadError String m) => TE.Env Type -> [Binding] -> m (TE.Env Type)
   extendEnv env []          = return env
   extendEnv env ((n, e):bs) = do
-    t <- typeOfExpression env e
+    t <- typeOfExpr env e
     extendEnv (env `TE.extend` (n, t)) bs
 
   recursiveExtend :: (MonadState (InterpreterState Type) m, MonadError String m) => TE.Env Type -> [Binding] -> m (TE.Env Type)
@@ -122,7 +122,7 @@ module Languages.EnrichedLambda.Typing where
     recursiveExtend' :: (MonadState (InterpreterState Type) m, MonadError String m) => TE.Env Type -> [String] -> [Binding] -> m (TE.Env Type)
     recursiveExtend' env []     []          = return env
     recursiveExtend' env []     ((n, e):bs) = do
-      t1 <- typeOfExpression env e
+      t1 <- typeOfExpr env e
       t2 <- n `TE.get` env
       addConstraint t1 t2
       recursiveExtend' env [] bs
@@ -135,7 +135,7 @@ module Languages.EnrichedLambda.Typing where
   typeOfClause env (tp, cs, vs, e) = do
     t1  <- tagToType tp
     tvs <- constrTagToVars tp cs
-    t2  <- typeOfExpression ((zip vs tvs) ++ env) e
+    t2  <- typeOfExpr ((zip vs tvs) ++ env) e
     return $ T_Arrow t1 t2
 
   typeOfCase :: (MonadState (InterpreterState Type) m, MonadError String m) => TE.Env Type -> [Clause] -> m Type
@@ -146,67 +146,67 @@ module Languages.EnrichedLambda.Typing where
     addConstraints $  map (\x -> (v, x)) ts
     return v
 
-  typeOfExpression :: (MonadState (InterpreterState Type) m, MonadError String m) => TE.Env Type -> Expr -> m Type
-  typeOfExpression env (E_UPrim up)     =
+  typeOfExpr :: (MonadState (InterpreterState Type) m, MonadError String m) => TE.Env Type -> Expr -> m Type
+  typeOfExpr env (E_UPrim up)     =
     typeOfUnaryPrim up
-  typeOfExpression env (E_BPrim bp)     =
+  typeOfExpr env (E_BPrim bp)     =
     typeOfBinaryPrim bp
-  typeOfExpression env (E_Val n)        =
+  typeOfExpr env (E_Val n)        =
     n `TE.get` env
-  typeOfExpression env (E_Num _)        =
+  typeOfExpr env (E_Num _)        =
     return $ T_Int
-  typeOfExpression env (E_Constr 0 0 0) =
+  typeOfExpr env (E_Constr 0 0 0) =
     return $ boolType
-  typeOfExpression env (E_Constr 0 1 0) =
+  typeOfExpr env (E_Constr 0 1 0) =
     return $ boolType
-  typeOfExpression env (E_Constr 1 0 0) =
+  typeOfExpr env (E_Constr 1 0 0) =
     return $ unitType
-  typeOfExpression env (E_Constr 2 0 0) = do
+  typeOfExpr env (E_Constr 2 0 0) = do
     v <- newVar
     return $ listType $ T_Var v
-  typeOfExpression env (E_Constr 2 0 2) = do
+  typeOfExpr env (E_Constr 2 0 2) = do
     a <- newVar
     let v = T_Var a
     return $ T_Arrow v $ T_Arrow (listType v) $ listType v
-  typeOfExpression env (E_Constr 3 0 2) = do
+  typeOfExpr env (E_Constr 3 0 2) = do
     a <- newVar
     b <- newVar
     let v1 = T_Var a
     let v2 = T_Var b
     return $ T_Arrow v1 $ T_Arrow v2 $ pairType v1 v2
-  typeOfExpression env (E_Seq e1 e2)    = do
-    t1 <- typeOfExpression env e1
+  typeOfExpr env (E_Seq e1 e2)    = do
+    t1 <- typeOfExpr env e1
     addConstraint t1 unitType
-    typeOfExpression env e2
-  typeOfExpression env (E_Apply e1 e2)  = do
-    t1 <- typeOfExpression env e1
-    t2 <- typeOfExpression env e2
+    typeOfExpr env e2
+  typeOfExpr env (E_Apply e1 e2)  = do
+    t1 <- typeOfExpr env e1
+    t2 <- typeOfExpr env e2
     a <- newVar
     let v = T_Var a
     addConstraint t1 $ T_Arrow t2 v
     return v
-  typeOfExpression env (E_Rescue e1 e2) = do
-    t1 <- typeOfExpression env e1
-    t2 <- typeOfExpression env e2
+  typeOfExpr env (E_Rescue e1 e2) = do
+    t1 <- typeOfExpr env e1
+    t2 <- typeOfExpr env e2
     addConstraint t1 t2
     return t1
-  typeOfExpression env (E_Let bs e)     = do
+  typeOfExpr env (E_Let bs e)     = do
     env' <- extendEnv env bs
-    typeOfExpression env' e
-  typeOfExpression env (E_LetRec bs e)  = do
+    typeOfExpr env' e
+  typeOfExpr env (E_LetRec bs e)  = do
     env' <- recursiveExtend env bs
-    typeOfExpression env' e
-  typeOfExpression env (E_Case e cs)    = do
-    t               <- typeOfExpression env e
+    typeOfExpr env' e
+  typeOfExpr env (E_Case e cs)    = do
+    t               <- typeOfExpr env e
     (T_Arrow t1 t2) <- typeOfCase env cs
     addConstraint t t1
     return t2
-  typeOfExpression env (E_Function n e) = do
+  typeOfExpr env (E_Function n e) = do
     a <- newVar
     let v = T_Var a
-    t <- typeOfExpression (env `TE.extend` (n, v)) e
+    t <- typeOfExpr (env `TE.extend` (n, v)) e
     return $ T_Arrow v t
-  typeOfExpression env E_MatchFailure   = do
+  typeOfExpr env E_MatchFailure   = do
     a <- newVar
     return $ T_Var a
 
@@ -214,14 +214,29 @@ module Languages.EnrichedLambda.Typing where
   typeOfDefinition env (D_Let bs)    = extendEnv env bs
   typeOfDefinition env (D_LetRec bs) = recursiveExtend env bs
 
-  typeOfProgram :: (MonadState (InterpreterState Type) m, MonadError String m) => TE.Env Type -> Program -> m Type
-  typeOfProgram env ([], e)     = do
-    t <- typeOfExpression env e
+  typeOfInstr :: (MonadState (InterpreterState Type) m, MonadError String m) => TE.Env Type -> Instruction -> m (Maybe Type, TE.Env Type)
+  typeOfInstr env (IDF df) = do
+    env' <- typeOfDefinition env df
+    return (Nothing, env')
+  typeOfInstr env (IEX ex) = do
+    t <- typeOfExpr env ex
     s <- unify
-    return $ t `TC.applySubst` s
-  typeOfProgram env ((d:ds), e) = do
-    env' <- typeOfDefinition env d
-    typeOfProgram env' (ds, e)
+    return $ (Just $ t `TC.applySubst` s, map (\(n, t) -> (n, t `TC.applySubst` s)) env)
 
-  typeOf :: TE.Env Type -> Program -> Either String Type
-  typeOf env p = fst $ runState (runErrorT (typeOfProgram env p)) emptyState
+  typeOfProg :: (MonadState (InterpreterState Type) m, MonadError String m) => TE.Env Type -> Program -> m (Type, TE.Env Type)
+  typeOfProg env ([], e)     = do
+    t <- typeOfExpr env e
+    s <- unify
+    return $ (t `TC.applySubst` s, map (\(n, t) -> (n, t `TC.applySubst` s)) env)
+  typeOfProg env ((d:ds), e) = do
+    env' <- typeOfDefinition env d
+    typeOfProg env' (ds, e)
+
+  typeOfExpression :: TE.Env Type -> Expr -> Either String Type
+  typeOfExpression env e = fst $ runState (runErrorT (do { t <- typeOfExpr env e; s <- unify; return $ t `TC.applySubst` s})) emptyState
+
+  typeOfInstruction :: TE.Env Type -> Instruction -> Either String (Maybe Type, TE.Env Type)
+  typeOfInstruction env instr = fst $ runState (runErrorT (typeOfInstr env instr)) emptyState
+
+  typeOfProgram :: TE.Env Type -> Program -> Either String (Type, TE.Env Type)
+  typeOfProgram env p = fst $ runState (runErrorT (typeOfProg env p)) emptyState
