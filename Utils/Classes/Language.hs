4f2a8ddbfd4cccabcade3ee84f6b643f966752f9
Krzysztof Sakwerda <ksakwerda@gmail.com>
2012-11-13 23:51:49 +0100

    + Correct Language typeclass design according to needs of interpreter

--- Utils/Classes/Language.hs
+++ Utils/Classes/Language.hs
@@ -5,8 +5,8 @@
 
 module Utils.Classes.Language where
   import Utils.Classes.Clojure
-  --import Utils.Classes.Definition
   import Utils.Classes.Expression
+  import Utils.Classes.Instruction
   import Utils.Classes.Program
   import Utils.Classes.Type
   import Utils.Classes.Value
@@ -17,7 +17,12 @@ module Utils.Classes.Language where
   import Control.Monad.Error
   import Text.Parsec.Error
 
-  class (Program p, Type tp, Expression e, Value v, Clojure v e) => Language p tp e v where
-    typeOf  :: TE.Env tp -> p -> Either String tp
-    eval    :: Memory v -> EE.Env v e -> p -> Either String (v, Memory v)
-    parser  :: String -> Either ParseError p
+  class (Clojure v e, Expression e, Instruction i, Program p, Type tp, Value v) => Language p tp e i v where
+    typeOfExpression     :: TE.Env tp -> e -> Either String tp
+    typeOfInstruction    :: TE.Env tp -> i -> Either String (Maybe tp, TE.Env tp)
+    typeOfProgram        :: TE.Env tp -> p -> Either String (tp, TE.Env tp)
+    evalInstruction      :: Memory v -> EE.Env v e -> i -> Either String (Maybe v, EE.Env v e, Memory v)
+    evalProgram          :: Memory v -> EE.Env v e -> p -> Either String (v, EE.Env v e, Memory v)
+    parseExpression      :: String -> Either ParseError e
+    parseInstruction     :: String -> Either ParseError i
+    parseProgramFromFile :: String -> IO (Either ParseError p)
